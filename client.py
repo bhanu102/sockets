2719486ffeaf46cc9f2ad14277dd22b6a96529b3
bhanu102 <bhanugadupudi45@gmail.com>
2022-01-27 17:26:13 +0530

    initial

--- client.py
+++ client.py
@@ -1,7 +1,29 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+HEADERSIZE = 10
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1243))
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new msg len:",msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        print(f"full message length: {msglen}")
+
+        full_msg += msg.decode("utf-8")
+
+        print(len(full_msg))
+
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ""
